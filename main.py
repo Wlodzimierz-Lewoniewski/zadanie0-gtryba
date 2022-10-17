bd2e22e6ab420379b57ebfdad979661257e79533
gtryba <72496773+gtryba@users.noreply.github.com>
2022-10-17 15:09:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+import string
+lista=[]
 
-if __name__ == "__main__":
-    pass
+liczba_dokumentow = int(input())
+dokumenty = []
+
+for dok in range(liczba_dokumentow):
+  dok = input()
+  dokumenty.append(dok)
+
+zapytania = []
+liczba_zapytan = int(input())
+for zap in range(liczba_zapytan):
+  zap = input()
+  zapytania.append(zap)
+
+for zapytanie in zapytania: 
+  interpunkcje = string.punctuation
+  wynik=[]
+  s={}
+  for num,dok in enumerate(dokumenty):
+    for i in interpunkcje:
+      dok=dok.replace(i, "")
+
+    dok=dok.replace(" ", " ")
+    tokeny=dok.lower().strip().split()
+    if zapytanie in tokeny: 
+      s[num]=tokeny.count(zapytanie)
+
+  for w in sorted(s, key=s.get, reverse=True):
+    wynik.append(w)
+
+  print(wynik)
